b2fd0d22f5965899b7ff16e722c940e68b9c824d
Manish Gautam <manish.gautam_cs18@gla.ac.in>
2020-03-28 02:13:20 +0530

    Update MyMain.java
    execution class.

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -8,4 +8,25 @@ package problem1.main;
 // executable class
 
 public class MyMain {
+public static void main(String[] args) {
+        MyBinarySearchTree bst=new MyBinarySearchTree();
+        bst.insert(100);
+        bst.insert(50);
+        bst.insert(150);
+        bst.insert(200);
+        bst.insert(250);
+        bst.insert(300);
+        bst.insert(270);
+        bst.insert(180);
+        bst.insert(60);
+        bst.traversepreOrder(bst.getRoot());
+        System.out.println();
+        bst.traverseinOrder(bst.getRoot());
+        System.out.println();
+        bst.traversepostOrder(bst.getRoot());
+        System.out.println(bst.search(100));
+        bst.countnodes();
+        bst.leftView(root);
+    }
+
 }
