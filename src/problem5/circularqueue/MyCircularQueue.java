c7a0eb34b52e2ed9b0edaafd7586fe0d7feb7dce
Manish Gautam <manish.gautam_cs18@gla.ac.in>
2020-03-27 20:35:49 +0530

    Update MyCircularQueue.java
    enqueue

--- src/problem5/circularqueue/MyCircularQueue.java
+++ src/problem5/circularqueue/MyCircularQueue.java
@@ -8,44 +8,33 @@ package problem5.circularqueue;
 import problem5.node.Node;
 import problem5.student.Student
 
-import java.util.ArrayList;
 //to implement circular queue
 public class MyCircularQueue {
-    private Node front;
-    private int size;
+    Node front;
+    Node rear;
+    int count;
 
-    public MyCircularQueue() {
+    public MyCircularQueue(){
         front = null;
-        size = 0;
+        rear = null;
+        count = 0;
     }
 
-      public void enqueue(ArrayList<Student> addData) {
-        for (Student s : addData) {
-            if (s.getBackLogCounter() - s.getAppearingCounter() == 0) {
-            } 
-            else if (s.getBackLogCounter() - s.getAppearingCounter() > 0) {
-                
-                Node temp = new Node(s);
-                if (this.front == null) {
-                    front = temp;
-                    temp.setNext(temp);
-                    size++;
-                } else {
-                    temp.setNext(this.front);
-                    Node parser = this.front;
-                    while (parser.getNext() != this.front) {
-                        parser = parser.getNext();
-                    }
-                    parser.setNext(temp);
-                    front = temp;
-                    size++;
-                }
-            } else {
-                //Invalid Student Data
-                System.out.println("Given Data is not Valid");
-            }
+
+
+      public void enqueue(int rollNumber,int backLogCounter,int appearingCounter){
+        Student student = new Student(rollNumber,backLogCounter,appearingCounter);
+        Node node = new Node();
+        node.setStudent(student);
+
+        if(front==null){
+            front = node;
+        }
+        else{
+            rear.setNext(node);
         }
 
+        rear = node;
+        rear.setNext(front);
+        count++;
     }
-
-}
