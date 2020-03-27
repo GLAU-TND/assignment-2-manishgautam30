562006f7212810abfae9c1be3d8212bb86c78e2d
Manish Gautam <manish.gautam_cs18@gla.ac.in>
2020-03-27 21:29:37 +0530

    Update TreeNode.java
    taking a left and a right node initialized to null.

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -7,4 +7,14 @@
 package problem1.node;
 
 public class TreeNode {
+    private int data;
+    private TreeNode left;
+    private TreeNode right;
+
+    public TreeNode(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+
 }
