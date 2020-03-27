36671b19240066286d49cd622f3d881d47651815
Manish Gautam <manish.gautam_cs18@gla.ac.in>
2020-03-27 19:13:22 +0530

    Update Student.java
    created getter setter of roll number , name,backlog counter and appearing counter

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -6,7 +6,9 @@
  */
 package problem5.student;
 // to store student information and properties
+
 public class Student {
+
   private int rollNumber;
   private String Name;
   private int backLogCounter;
@@ -18,7 +20,36 @@ public class Student {
     Name=name;
     this.backLogCounter=backLogCounter;
     this.appearingCounter=appearingCounter;
- }
+    }
+    public int getrollNumber(){
+     return rollNumber;
+    }
     
+    public String getName(){
+     return Name;
+    }
     
+    public int getbackLogCounter(){
+     return backLogCounter;
+    }
+    
+    public int getappearingCounter(){
+     return appearingCounter;
+    }
+
+    
+    public void setrollNumber(int rollNumber){
+     this.rollNumber=rollNumber;
+    }
+    
+    public void setName(String name){
+     Name=name;
+    }
+
+    public void setbackLogCounter(int backLogCounter){
+     this.backLogCounter=backLogCounter;
+    }
+    public void setappearingCounter(int appearingCounter){
+     this.appearingCounter=appearingCounter;
+    }
 }
