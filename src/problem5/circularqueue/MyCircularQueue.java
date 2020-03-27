9d390838bd96d13c5c31ac9e6cf1c04e3c672cf7
Manish Gautam <manish.gautam_cs18@gla.ac.in>
2020-03-27 20:14:55 +0530

    Update MyCircularQueue.java
    created constructor and  done initialization.

--- src/problem5/circularqueue/MyCircularQueue.java
+++ src/problem5/circularqueue/MyCircularQueue.java
@@ -5,6 +5,16 @@
  *  Time: 7:06 PM
  */
 package problem5.circularqueue;
+import problem5.node.Node;
+import problem5.student.Student
+
+import java.util.ArrayList;
 //to implement circular queue
 public class MyCircularQueue {
-}
+    private Node front;
+    private int size;
+
+    public MyCircularQueue() {
+        front = null;
+        size = 0;
+    }
