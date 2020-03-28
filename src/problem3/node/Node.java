05448cf07183f0da059dfd2cb84e9c5f44bc5187
Manish Gautam <manish.gautam_cs18@gla.ac.in>
2020-03-28 21:19:04 +0530

    Update Node.java
    getter setter for student

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -12,13 +12,18 @@ public class Node {
   private Student student;
   private Node next;
 
-public Node(Student student) {
+    public Node(Student student) {
         this.student = student;
         next = null;
     }
 
+  
+    public Student getData() {
+        return student;
+    }
 
-
-
+    public void setData(Student student) {
+        this.student = student;
+    }
 
 }
