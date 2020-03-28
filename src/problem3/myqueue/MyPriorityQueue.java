520439e30ab39934c31b0f637612ef28b26d6f61
Manish Gautam <manish.gautam_cs18@gla.ac.in>
2020-03-28 21:30:21 +0530

    Update MyPriorityQueue.java
    enqueue method.

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -13,9 +13,40 @@ public class MyPriorityQueue {
     private Node front;
     private int size;
 
-    
     public MyPriorityQueue() {
         front = null;
         rear = null;
         size = 0;
+    }
+  
+    public void enqueue(Node addData) {
+        if (front == null) {
+            rear = addData;
+            front = addData;
+            size++;
+        } else {
+            if (addData.getData().getRollNumber() >= this.rear.getData().getRollNumber()) {
+                rear.setNext(addData);
+                rear = addData;
+                size++;
+            } else if (addData.getData().getRollNumber() <= this.front.getData().getRollNumber()) {
+                addData.setNext(this.front);
+                this.front = addData;
+                size++;
+            } else {
+                Node temp = this.front;
+                Node tempPrevious = this.front;
+                while (temp.getNext() != null) {
+                    if (temp.getData().getRollNumber() > addData.getData().getRollNumber()) {
+                        break;
+                    }
+                    tempPrevious = temp;
+                    temp = temp.getNext();
+                }
+                tempPrevious.setNext(addData);
+                addData.setNext(temp);
+                size++;
+
+            }
+        }
     }
